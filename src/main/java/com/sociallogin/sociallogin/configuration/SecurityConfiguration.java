9d758dcee15b2988ac1416039e2ec66671c1c94f
Wavhudi Mulaudzi <wavhudi.mulaudzi@impact.com>
2024-01-31 13:08:22 +0200

    First commit

---
+++ src/main/java/com/sociallogin/sociallogin/configuration/SecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.sociallogin.sociallogin.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
+
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(httpRequests -> httpRequests
+                        .anyRequest().authenticated())
+                .oauth2Login(Customizer.withDefaults());
+
+        return http.build();
+
+    }
+}
